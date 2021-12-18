67b25778e8a510f95ab92c2de7d4a04073b81315
nevada25 <calderon200396@gmail.com>
2021-12-18 14:32:08 -0500

    proyecto inicial listo

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink
+} from "react-router-dom";
+import logo from '../logo.svg'
+
+export const Navigation = () => {
+    return (
+        <Router>
+            <div className="main-layout">
+                <nav>
+                    <img src={logo} alt="React Logo"/>
+                    <ul>
+                        <li>
+                            <NavLink exact activeClassName="nav-active" to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact activeClassName="nav-active" to="/about">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact activeClassName="nav-active" to="/users">Users</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <h1>About</h1>
+                    </Route>
+                    <Route path="/users">
+                        <h1>users</h1>
+                    </Route>
+                    <Route path="/">
+                        <h1>/</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
